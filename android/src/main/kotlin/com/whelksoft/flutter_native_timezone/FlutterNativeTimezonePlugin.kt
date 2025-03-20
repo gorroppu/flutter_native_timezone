4280c9f6f0a15d9ee50fd0ad11ed7338f1e56042
gorroppu <tom.pepper@outlook.com>
2025-03-20 20:39:08 +0000

    Updated FlutterNativeTimezonePlugin.kt to v2

--- android/src/main/kotlin/com/whelksoft/flutter_native_timezone/FlutterNativeTimezonePlugin.kt
+++ android/src/main/kotlin/com/whelksoft/flutter_native_timezone/FlutterNativeTimezonePlugin.kt
@@ -4,27 +4,17 @@ import android.os.Build
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.BinaryMessenger
-import java.time.ZoneId
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
-import java.util.*
+import java.time.ZoneId
+import java.util.TimeZone
 import kotlin.collections.ArrayList
 
 class FlutterNativeTimezonePlugin : FlutterPlugin, MethodCallHandler {
 
     private lateinit var channel: MethodChannel
-
-    // backward compatibility with flutter api v1
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val plugin = FlutterNativeTimezonePlugin()
-            plugin.setupMethodChannel(registrar.messenger())
-        }
-    }
 
     override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         setupMethodChannel(binding.binaryMessenger)
@@ -37,9 +27,7 @@ class FlutterNativeTimezonePlugin : FlutterPlugin, MethodCallHandler {
     override fun onMethodCall(call: MethodCall, result: Result) {
         when (call.method) {
             "getLocalTimezone" -> result.success(getLocalTimezone())
-
             "getAvailableTimezones" -> result.success(getAvailableTimezones())
-
             else -> result.notImplemented()
         }
     }
